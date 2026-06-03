ceb7ef8e746c79624e6a5435ab8180b385767da7
Gustaw Molduano - Klein <gustaw.molduano@gmail.com>
2026-06-03 23:32:55 +0200

    Add files via upload

---
+++ fig/Triangle.java
@@ -0,0 +1,21 @@
+package fig;
+
+class Triangle {
+    private double a;
+
+    public Triangle(double a) {
+        this.a = a;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public double getArea() {
+        return (a * a * Math.sqrt(3)) / 4.0;
+    }
+
+    public void showArea() {
+        System.out.println("Pole powierzchni: " + getArea());
+    }
+}
